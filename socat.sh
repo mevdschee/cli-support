b6cf9f444525547d8b68c2bd05aa02e923280f33
Maurits van der Schee <maurits@vdschee.nl>
2017-08-18 00:02:28 +0200

    Send SIGWINCH every second

--- socat.sh
+++ socat.sh
@@ -21,8 +21,20 @@ src_dir="${1:?Usage $0 [user@hostname]}"
 ssh -R $PORT:localhost:$PORT -N $@ &
 PID_SSH=$!
 tmux new-session -d -s $SESSION
+TTY1=$(tty)
 set -- $(stty size) # $1 = rows $2 = columns
-socat system:"stty rows $1; stty cols $2; tmux attach -t $SESSION",pty,raw,echo=0,stderr,setsid,sigint tcp-listen:$PORT,bind=localhost,reuseaddr &
+CMD="stty rows $1;\
+     stty cols $2;\
+     TTY2=\$(tty);\
+     while sleep 1;\
+     do\
+       set -- \$(stty -F $TTY1 size);\
+       stty -F \$TTY2 rows \$1;\
+       stty -F \$TTY2 cols \$2;\
+       kill -28 \$\$;\
+     done"
+echo "$CMD & tmux attach -t $SESSION"
+socat system:"$CMD & tmux attach -t $SESSION",pty,raw,echo=0,stderr,setsid,sigint tcp-listen:$PORT,bind=localhost,reuseaddr &
 PID_SOCAT=$!
 tmux attach -t $SESSION
 tmux kill-session -t $SESSION >/dev/null 2>&1
